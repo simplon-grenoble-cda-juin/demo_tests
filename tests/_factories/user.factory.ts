460bffd077d9d8eeb0766aa0c51949c4ac8e20d8
Kew <wolff.kevin@protonmail.com>
2025-10-15 13:37:04 +0200

    Finalize tests project

--- tests/_factories/user.factory.ts
+++ tests/_factories/user.factory.ts
@@ -10,6 +10,8 @@ export type UserProps = {
 };
 
 export class UserFactory {
+  // Créer un User avec des données précises si fournis,
+  // autrement des données aléatoires
   static build = async (props: UserProps = {}): Promise<User> => {
     const email = props.email ?? faker.internet.email().toLowerCase();
     const password = props.password ?? (await argon2.hash("123"));
@@ -21,12 +23,15 @@ export class UserFactory {
     return new User(email, password, created_at);
   };
 
+  // Créer un User avec des données précises (non optionnel)
   static populate = async (
     client: Client,
     props: UserProps = {}
   ): Promise<User> => {
+    // Création du User
     const user = await this.build(props);
 
+    // Exécution de la requête en BDD
     const res = await client.query<{ id: number }>(
       `INSERT INTO public."user"(email, password, created_at)
         VALUES ($1, $2, $3)
@@ -35,11 +40,14 @@ export class UserFactory {
       [user.getEmail(), user.getPasswordHash(), user.getCreatedAt()]
     );
 
+    // On récupère l'ID du User créé et on l'ajoute à la classe
+    // qui représente notre User
     user.setId(res.rows[0].id);
 
     return user;
   };
 
+  // Créer X User avec des données aléatoires
   static populateRandom = async (
     client: Client,
     number: number
